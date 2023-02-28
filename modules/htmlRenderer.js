a85cfeb89bec48a5f84f0f6d772f227ada8c506f
Borna Grilec <borna@develo.agency>
2023-02-28 12:01:18 +0100

    Added polaznici search & sorting functionality

--- modules/htmlRenderer.js
+++ modules/htmlRenderer.js
@@ -1,31 +1,73 @@
-export const renderItems = (lectures, fallbackMessage = "No results") => {
-  const gridEl = document.querySelector(".grid");
+export const renderItems = (items, fallbackMessage = "No results") => {
+  const tableEl = document.querySelector(".table");
+  const tableBodyEl = document.querySelector(".table__body");
   const alertEl = document.querySelector(".alert");
 
-  gridEl.innerHTML = "";
+  tableBodyEl.innerHTML = "";
 
-  if (lectures.length) {
+  if (items.length) {
     alertEl.classList.remove("alert_visible");
 
-    lectures.forEach((lecture) => {
-      const cardEl = document.createElement("div");
-      cardEl.className = "card";
+    items.forEach((item) => {
+      tableEl.style.display = "table";
 
-      const cardTitleEl = document.createElement("h2");
-      cardTitleEl.className = "card__title";
-      cardTitleEl.innerText = lecture.lectureName;
+      const tableRowEl = document.createElement("tr");
+      tableRowEl.className = "table__row";
 
-      const cardLengthEl = document.createElement("p");
-      cardLengthEl.className = "card__length";
-      cardLengthEl.innerText = lecture.lectureLength + " min";
+      const tableDataIdEl = document.createElement("td");
+      tableDataIdEl.className = "table__data";
+      tableDataIdEl.innerText = item.id;
 
-      cardEl.appendChild(cardTitleEl);
-      cardEl.appendChild(cardLengthEl);
+      const tableDataNameEl = document.createElement("td");
+      tableDataNameEl.className = "table__data";
+      tableDataNameEl.innerText = item.name;
 
-      gridEl.appendChild(cardEl);
+      const tableDataSurnameEl = document.createElement("td");
+      tableDataSurnameEl.className = "table__data";
+      tableDataSurnameEl.innerText = item.surname;
+
+      const tableDataAgeEl = document.createElement("td");
+      tableDataAgeEl.className = "table__data";
+      tableDataAgeEl.innerText = item.age;
+
+      tableRowEl.appendChild(tableDataIdEl);
+      tableRowEl.appendChild(tableDataNameEl);
+      tableRowEl.appendChild(tableDataSurnameEl);
+      tableRowEl.appendChild(tableDataAgeEl);
+
+      tableBodyEl.appendChild(tableRowEl);
     });
   } else {
+    tableEl.style.display = "none";
     alertEl.classList.add("alert_visible");
     alertEl.innerText = fallbackMessage;
   }
+};
+
+export const handleSearch = (attendees, searchTerm, filterBy) => {
+  let filteredResults = [];
+
+  switch (filterBy) {
+    case "":
+      filteredResults = attendees.filter((attendee) =>
+        attendee.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      break;
+    case "age_ascending":
+      filteredResults = attendees
+        .filter((attendee) =>
+          attendee.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .sort((a, b) => a.age - b.age);
+      break;
+    case "age_descending":
+      filteredResults = attendees
+        .filter((attendee) =>
+          attendee.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .sort((a, b) => b.age - a.age);
+      break;
+  }
+
+  return filteredResults;
 };
--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 // Imports
-import { lectures } from "./data/data.js";
-import { renderItems } from "./modules/htmlRenderer.js";
+import { attendees } from "./data/data.js";
+import { renderItems, handleSearch } from "./modules/htmlRenderer.js";
 // Variables
 const inputSearchEl = document.querySelector(".search-input");
+const selectEl = document.querySelector(".select");
 
 // Functions that are executed on body load
-renderItems(lectures);
+renderItems(attendees);
 
 // Event listeners
 inputSearchEl.addEventListener("keyup", (event) => {
   const searchTerm = event.target.value;
+  const filterBy = selectEl.value;
 
-  const filteredResults = lectures.filter((lecture) =>
-    lecture.lectureName.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredResults = handleSearch(attendees, searchTerm, filterBy);
 
   renderItems(
     filteredResults,
-    "There are no lessons according to the entered term"
+    "There are no attendees according to the entered term"
+  );
+});
+
+selectEl.addEventListener("change", (event) => {
+  const searchTerm = inputSearchEl.value;
+  const filterBy = event.target.value;
+
+  const filteredResults = handleSearch(attendees, searchTerm, filterBy);
+
+  renderItems(
+    filteredResults,
+    "There are no attendees according to the entered term"
   );
 });
